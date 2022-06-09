94fd9a566bc8bf9af6383d818a03312ad7a0cd85
Alvin So <r.alvso@icloud.com>
2022-06-09 13:45:04 +0800

    Fix a bug

--- src/thread_manager.py
+++ src/thread_manager.py
@@ -92,18 +92,18 @@ class ThreadManager:
         self._exitEvent.set()
 
     def ping(
-            self, call: Callable, exitEvent: threading.Event = threading.Event(), interval: float = 1, *args
+            self, call: Callable, exitEvent: threading.Event = threading.Event(), interval: float = 1
     ) -> threading.Event:
-        args = (call, exitEvent, interval,) + args
+        args = (call, exitEvent, interval,)
         self.execute(self._pinger, args=args, threadType=ThreadType.PING_THREAD)
         return exitEvent
 
-    def _pinger(self, call: Callable, exitEvent: threading.Event, interval: float = 1, *args, **kwargs) -> None:
+    def _pinger(self, call: Callable, exitEvent: threading.Event, interval: float = 1) -> None:
         while True:
             if exitEvent.is_set():
                 return
             sleep(interval)
-            call(*args, **kwargs)
+            call()
 
     def _addCounter(self, threadType: str) -> int:
         counterList: List[int] = self._threadCounters.get(threadType, None)
