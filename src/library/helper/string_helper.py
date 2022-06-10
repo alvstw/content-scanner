ac17b3599f8972ca32a3237bc34df78fe53cf892
Alvin So <r.alvso@icloud.com>
2022-06-10 10:03:00 +0800

    Fix a bug with console string filtering

--- src/library/helper/string_helper.py
+++ src/library/helper/string_helper.py
@@ -3,5 +3,5 @@ import re
 
 class StringHelper:
     @staticmethod
-    def filterString(testString: str, rule: str = r'[^A-Za-z\d_@.\\\/#&+-]+') -> str:
+    def filterString(testString: str, rule: str = r'[^A-Za-z\d_@.\s\\\/#&+-]+') -> str:
         return re.sub(rule, '', testString)
